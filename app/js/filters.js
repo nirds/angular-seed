79b3d43c3a40724e749ff2c61349ebd236bdc496
Christopher Spring <chris.spring@gmail.com>
2013-08-09 20:05:58 +0200

    reverse filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,10 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).
+  filter('reverse', function(){
+    return function(text){
+      text = text || ""
+      return text.split("").reverse().join("");
+    }
+  });
